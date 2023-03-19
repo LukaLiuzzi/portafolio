60439b94960edfc318edfacdfdcee96bef0f5b5c
Luka Liuzzi <lukaliuzzidev@gmail.com>
2023-03-19 01:42:32 -0300

    Add cv and fixes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,23 +4,29 @@ import SectionWrapper from './SectionWrapper';
 const About: React.FC = () => {
 	return (
 		<SectionWrapper id='about'>
-			<h2 className='text-4xl font-bold'>Sobre mi</h2>
-			<p className='mt-4 text-lg'>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus hic
-				commodi sint architecto laboriosam molestias magni, perferendis
-				similique impedit doloremque error harum explicabo blanditiis,
-				consequuntur ea numquam excepturi ipsum. At.
-			</p>
-			<button className='mt-6 dark:bg-green-300 px-4 py-2 rounded-md text-base dark:text-dark-primary font-bold uppercase'>
-				Descargar CV
-			</button>
+			<h2 className='text-4xl font-bold text-center'>Sobre mi</h2>
+			<div className='flex flex-col justify-center items-center md:flex-row gap-10 md:mt-10 w-full'>
+				<div className='flex-1'>
+					<p className='mt-4 text-lg'>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+						hic commodi sint architecto laboriosam molestias magni, perferendis
+						similique impedit doloremque error harum explicabo blanditiis,
+						consequuntur ea numquam excepturi ipsum. At.
+					</p>
+					<a href='/CVLukaLiuzzi.pdf' download>
+						<button className='mt-6 dark:bg-green-300 px-4 py-2 rounded-md text-base dark:text-dark-primary font-bold uppercase'>
+							Descargar CV
+						</button>
+					</a>
+				</div>
 
-			<div className='mt-14 flex justify-center items-center'>
-				<img
-					src={Selfie}
-					alt='Foto de luka liuzzi, desarrollador web'
-					className='rounded w-64 h-64 object-cover'
-				/>
+				<div className='flex-1 flex justify-center items-center'>
+					<img
+						src={Selfie}
+						alt='Foto de luka liuzzi, desarrollador web'
+						className='rounded w-64 h-64 object-cover'
+					/>
+				</div>
 			</div>
 		</SectionWrapper>
 	);
