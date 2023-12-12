62cd9d7a26b513160d3d3417da1c505660054497
Luka <lukaliuzzidev@gmail.com>
2023-12-12 17:18:19 -0300

    Update cv - remove process

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ export default function App() {
               <About />
               <Skills />
               <Projects />
-              <Process />
+              {/* <Process /> */}
               <Contact />
             </main>
           </Layout>
